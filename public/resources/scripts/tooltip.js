4c08a87c0f8f9af3b9dbdd852c3c2ec7d4ab5731
Jérémie Dupuis <JeremieD@users.noreply.github.com>
2024-02-03 10:16:06 -0500

    Slightly intelligent tooltip position

--- public/resources/scripts/tooltip.js
+++ public/resources/scripts/tooltip.js
@@ -38,18 +38,44 @@ function showTooltip(el) {
   const elRect = el.getBoundingClientRect();
   const tooltipAlign = el.getAttribute("tooltip-align")?.trim() ?? "";
 
-  if (tooltipAlign == "top-right") {
+  const topRight = () => {
     tooltip.className = "top-right";
     tooltip.style.top = elRect.top - 20 + "px";
     tooltip.style.left = elRect.left + "px";
-  } else if (tooltipAlign == "left-middle") {
+  };
+  const bottomRight = () => {
+    tooltip.className = "bottom-right";
+    tooltip.style.top = elRect.bottom + 20 + "px";
+    tooltip.style.left = elRect.left + "px";
+  };
+  const leftMiddle = () => {
     tooltip.className = "left-middle";
     tooltip.style.top = elRect.top + elRect.height/2 + "px";
     tooltip.style.left = elRect.left - 20 + "px";
-  } else {
+  };
+  const topCenter = () => {
     tooltip.className = "top-center";
     tooltip.style.top = elRect.top - 20 + "px";
     tooltip.style.left = elRect.left + elRect.width/2 + "px";
+  };
+  const bottomCenter = () => {
+    tooltip.className = "bottom-center";
+    tooltip.style.top = elRect.bottom + 20 + "px";
+    tooltip.style.left = elRect.left + elRect.width/2 + "px";
+  };
+
+  if (tooltipAlign == "top-right") {
+    topRight();
+    const tooltipRect = tooltip.getBoundingClientRect();
+    if (tooltipRect.y < 8) bottomRight();
+
+  } else if (tooltipAlign == "left-middle") {
+    leftMiddle();
+
+  } else {
+    topCenter();
+    const tooltipRect = tooltip.getBoundingClientRect();
+    if (tooltipRect.y < 8) bottomCenter();
   }
 
   // Show tooltip
