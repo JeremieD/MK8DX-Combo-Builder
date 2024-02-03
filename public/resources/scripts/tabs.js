981c5fb6d938fab2142d8de370d26d900cfcad93
Jérémie Dupuis <JeremieD@users.noreply.github.com>
2024-02-03 16:37:08 -0500

    Tabs "accessibility"
    i mean it's still super jank but itll do for now

--- public/resources/scripts/tabs.js
+++ public/resources/scripts/tabs.js
@@ -1,7 +1,7 @@
 whenDOMReady(() => {
   for (const tabContainer of document.getElementsByClassName("tab-container")) {
     for (const handle of tabContainer.querySelectorAll(".tab-handles>a")) {
-      handle.addEventListener("click", e => {
+      const handler = e => {
         e.preventDefault();
         const tabID = handle.dataset.tabId;
         for (const handle of tabContainer.querySelectorAll(".tab-handles>a")) {
@@ -10,6 +10,10 @@ whenDOMReady(() => {
         for (const panel of tabContainer.querySelectorAll(".tab-panels>*")) {
           panel.classList.toggle("selected", panel.id == tabID);
         }
+      };
+      handle.addEventListener("click", handler);
+      handle.addEventListener("keydown", e => {
+        if (e.key == "Enter") handler(e);
       });
     }
   }
